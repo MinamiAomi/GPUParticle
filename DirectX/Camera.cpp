49218f732ff99c302d424f08cde3615e5ab7e7ee
MinamiAomi <k022g0040@g.neec.ac.jp>
2023-05-25 21:15:43 +0900

    c

--- DirectX/Camera.cpp
+++ DirectX/Camera.cpp
@@ -2,4 +2,7 @@
 #include "Camera.h"
 
 void Camera::UpdateMatrix() {
+	viewMatrix_ = Math::Inverse(Math::Matrix44::MakeAffine({ 1.0f,1.0f,1.0f }, rotate_, position_));
+	projectionMatrix_ = Math::Matrix44::MakePerspectiveProjection(fovY_, aspect_, nearZ_, farZ_);
+	viewProjectionMatrix_ = viewMatrix_ * projectionMatrix_;
 }
